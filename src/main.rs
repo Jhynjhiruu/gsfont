853c212392c245a2f9397b0b623b1f2b3a4c6a12
Jhynjhiruu <cub3r12@gmail.com>
2025-03-24 01:45:35 +0000

    Attempt to handle SWs properly

--- src/main.rs
+++ src/main.rs
@@ -51,15 +51,52 @@ enum Command {
     },
 }
 
-fn build_function(row: u8) -> String {
+fn build_function(row: u8, double: bool) -> String {
     let mut rv = String::new();
 
     rv += "    lw     s0, 0(a0)\n";
     rv += &format!("    addi   a0, a0, {}\n", size_of::<u32>());
 
-    for i in 0..u8::BITS {
-        if (row >> (u8::BITS - i - 1)) & 1 != 0 {
-            rv += &format!("    sh     s1, {}(a1)\n", i * size_of::<Pixel>() as u32);
+    for i in (0..u8::BITS).step_by(2) {
+        let pair = (row >> (u8::BITS - i - 1)) & 0b00000011;
+        if double {
+            match pair {
+                0b00 => {}
+                0b01 => {
+                    rv += &format!(
+                        "    sh     s1, {}(a1)\n",
+                        (i + 1) * size_of::<Pixel>() as u32
+                    );
+                }
+                0b10 => {
+                    rv += &format!("    sh     s1, {}(a1)\n", i * size_of::<Pixel>() as u32);
+                }
+                0b11 => {
+                    rv += &format!("    sw     s1, {}(a1)\n", i * size_of::<Pixel>() as u32);
+                }
+                _ => unreachable!(),
+            }
+        } else {
+            match pair {
+                0b00 => {}
+                0b01 => {
+                    rv += &format!(
+                        "    sh     s1, {}(a1)\n",
+                        (i + 1) * size_of::<Pixel>() as u32
+                    );
+                }
+                0b10 => {
+                    rv += &format!("    sh     s1, {}(a1)\n", i * size_of::<Pixel>() as u32);
+                }
+                0b11 => {
+                    rv += &format!("    sh     s1, {}(a1)\n", i * size_of::<Pixel>() as u32);
+                    rv += &format!(
+                        "    sh     s1, {}(a1)\n",
+                        (i + 1) * size_of::<Pixel>() as u32
+                    );
+                }
+                _ => unreachable!(),
+            }
         }
     }
 
@@ -75,11 +112,10 @@ fn build_function(row: u8) -> String {
 fn build(data: &[u8], first_label: &str, second_label: &str) -> Result<String> {
     let mut rv = String::from(PROLOGUE);
 
-    let mut odd = vec![];
-    let mut even = vec![];
+    let mut char_rows = vec![];
 
     for ch in data.chunks_exact(16 * 8) {
-        let mut buf = [0; 8 * 2];
+        let mut buf = [0; 8];
         for (index, row) in ch.chunks_exact(8).enumerate() {
             let mut b = 0;
 
@@ -90,68 +126,40 @@ fn build(data: &[u8], first_label: &str, second_label: &str) -> Result<String> {
             buf[index] = b;
         }
 
-        let o: [u8; 8] = buf[..8].try_into().unwrap();
-        let e: [u8; 8] = buf[8..].try_into().unwrap();
-
-        odd.push(o);
-        even.push(e);
+        char_rows.push(buf);
     }
 
-    let mut odd_rows = vec![];
+    let mut rows = vec![];
 
     for i in 0..(1 << 7) {
-        odd_rows.push(((i << 3) & 0b11111000) | ((i >> 4) & 0b00000110));
+        rows.push(((i << 3) & 0b11111000) | ((i >> 4) & 0b00000110));
     }
 
     let mut extra_rows = vec![];
 
-    let mut odd_strings = vec![];
+    let mut single_strings = vec![];
+    let mut double_strings = vec![];
 
-    for ch in &odd {
-        let mut o = vec![];
+    for ch in &char_rows {
+        let mut single_row = vec![];
+        let mut double_row = vec![];
         for i in ch {
-            if !odd_rows.contains(i) {
-                o.push(format!("row_extra_{i:08b}"));
+            if !rows.contains(i) {
+                single_row.push(format!("row_extra_{i:08b}"));
+                double_row.push(format!("row_extra_{i:08b}"));
                 if !extra_rows.contains(i) {
                     extra_rows.push(*i);
                 }
             } else {
-                o.push(format!("row_odd_{i:08b}"));
+                single_row.push(format!("row_single_{i:08b}"));
+                double_row.push(format!("row_double_{i:08b}"));
             }
         }
-        odd_strings.push(o);
+        single_strings.push(single_row);
+        double_strings.push(double_row);
     }
 
-    let mut even_rows = vec![];
-
-    for i in 0..(1 << 7) {
-        even_rows.push(((i << 3) & 0b11111000) | ((i >> 4) & 0b00000110));
-        /*println!(
-            "{}",
-            format!("{:08b}", even_rows.last().unwrap())
-                .replace('0', " ")
-                .replace('1', "█")
-        );*/
-    }
-
-    let mut even_strings = vec![];
-
-    for ch in &even {
-        let mut e = vec![];
-        for i in ch {
-            if !even_rows.contains(i) {
-                e.push(format!("row_extra_{i:08b}"));
-                if !extra_rows.contains(i) {
-                    extra_rows.push(*i);
-                }
-            } else {
-                e.push(format!("row_even_{i:08b}"));
-            }
-        }
-        even_strings.push(e);
-    }
-
-    for (index, (o, e)) in odd_strings.iter().zip(&even_strings).enumerate() {
+    for (index, (o, e)) in single_strings.iter().zip(&double_strings).enumerate() {
         if index == 0 {
             rv += &format!("EXPORT({})\n", first_label);
         }
@@ -173,26 +181,26 @@ fn build(data: &[u8], first_label: &str, second_label: &str) -> Result<String> {
         rv += "    .word row_end\n\n";
     }
 
-    for i in odd_rows {
-        let name = format!("row_odd_{i:08b}");
+    for &i in &rows {
+        let name = format!("row_single_{i:08b}");
         rv += &format!("LEAF({name})\n");
-        rv += &build_function(i);
+        rv += &build_function(i, false);
         rv += &format!("END({name})\n\n");
     }
 
     rv += ROW_END;
 
-    for i in even_rows {
-        let name = format!("row_even_{i:08b}");
+    for &i in &rows {
+        let name = format!("row_double_{i:08b}");
         rv += &format!("LEAF({name})\n");
-        rv += &build_function(i);
+        rv += &build_function(i, true);
         rv += &format!("END({name})\n\n");
     }
 
-    for i in extra_rows {
+    for &i in &extra_rows {
         let name = format!("row_extra_{i:08b}");
         rv += &format!("LEAF({name})\n");
-        rv += &build_function(i);
+        rv += &build_function(i, false);
         rv += &format!("END({name})\n\n");
     }
 
@@ -218,23 +226,10 @@ where
                 let w = instr & 0xFC000000 == 0xAC000000;
                 let offset = instr & 0x0000FFFF;
                 if w {
-                    if pixels[(offset >> 1) as usize] == 0 {
-                        pixels[(offset >> 1) as usize] = 0x7F;
-                    } else {
-                        pixels[(offset >> 1) as usize] = 0xBF;
-                    }
-
-                    if pixels[((offset >> 1) + 1) as usize] == 0 {
-                        pixels[((offset >> 1) + 1) as usize] = 0x7F;
-                    } else {
-                        pixels[((offset >> 1) + 1) as usize] = 0xBF;
-                    }
+                    pixels[(offset >> 1) as usize] = 0x7F;
+                    pixels[((offset >> 1) + 1) as usize] = 0x7F;
                 } else {
-                    if pixels[(offset >> 1) as usize] == 0 {
-                        pixels[(offset >> 1) as usize] = 0xFF;
-                    } else {
-                        pixels[(offset >> 1) as usize] = 0xBF;
-                    }
+                    pixels[(offset >> 1) as usize] = 0xFF;
                 }
             }
             Ok(Some(pixels.into_boxed_slice()))
@@ -261,14 +256,8 @@ fn extract(data: &[u8], vram: u32, num_chars: usize) -> Result<Vec<u8>> {
     let mut font: Vec<u8> = vec![];
 
     for chunk in offsets.chunks(9).collect::<Vec<_>>().chunks(2) {
-        if let [block, shadow] = chunk {
+        if let [block, _] = chunk {
             for offset in &block[..8] {
-                cursor.set_position(*offset as u64);
-                if let Some(l) = parse_function(&mut cursor)? {
-                    font.extend(l.iter());
-                }
-            }
-            for offset in &shadow[..8] {
                 cursor.set_position(*offset as u64);
                 if let Some(l) = parse_function(&mut cursor)? {
                     font.extend(l.iter());
@@ -290,7 +279,7 @@ fn main() -> Result<()> {
         } => {
             let infile = image::open(args.infile)?;
             assert_eq!(infile.width(), 8);
-            assert_eq!(infile.height() % 16, 0);
+            assert_eq!(infile.height() % 8, 0);
             let bw = infile.to_luma8();
             let out = build(bw.as_bytes(), &first_label, &second_label)?;
 
